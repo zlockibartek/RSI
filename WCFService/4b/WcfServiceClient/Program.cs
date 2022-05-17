08b49dceb5e4d8432aa05c410ec6e4c0531d61e7
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2022-05-17 10:38:55 +0200

    reports update

---
+++ WCFService/4b/WcfServiceClient/Program.cs
@@ -0,0 +1,151 @@
+﻿using CallbackService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WcfService2;
+using WcfServiceClient.ServiceReference2;
+using WcfServiceClient.ServiceReference2;
+using WcfServiceClient.ServiceReference3;
+
+namespace WcfServiceClient
+{
+    class MyData
+    {
+        public static void static_info()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Paweł Kolman 256778");
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.OSVersion.ToString());
+            Console.WriteLine(Environment.Version.ToString());
+            Console.WriteLine(Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString());
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData.static_info();
+            SuperCalcCallback myCbHandler = new SuperCalcCallback();
+            InstanceContext instanceContext = new InstanceContext(myCbHandler);
+            while(true)
+            {
+                Console.WriteLine("Choose action:");
+                Console.WriteLine("0. Leave");
+                Console.WriteLine("1. Add complex numbers");
+                Console.WriteLine("2. Mult complex numbers");
+                Console.WriteLine("3. Div complex numbers");
+                Console.WriteLine("4. Factorial");
+                Console.WriteLine("5. Remove record");
+                Console.WriteLine("6. Add Record");
+                Console.WriteLine("7. Count Records");
+
+                var leave = false;
+                var choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        leave = true;
+                        break;
+                    case 1:
+                        ServiceReference1.CalculatorClient client1 = new ServiceReference1.CalculatorClient("WSHttpBinding_ICalculator");
+                        Console.WriteLine("Type first number:\nReal:");
+                        var real1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imag1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Type first number:\nReal:");
+                        var real2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imag2 = float.Parse(Console.ReadLine());
+
+                        ComplexNum cnum1 = new ComplexNum(real1, imag1);
+                        ComplexNum cnum2 = new ComplexNum(real2, imag2);
+                        ComplexNum result1 = client1.addCNum(cnum1, cnum2);
+                        Console.WriteLine(" addCNum(...) = ({0}; {1})", result1.real, result1.imag);
+                        client1.Close();
+                        break;
+                    case 2:
+                        ServiceReference1.CalculatorClient clientMult = new ServiceReference1.CalculatorClient("WSHttpBinding_ICalculator");
+                        Console.WriteLine("Type first number:\nReal:");
+                        var realMult1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imagMult1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Type first number:\nReal:");
+                        var realMult2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imagMult2 = float.Parse(Console.ReadLine());
+
+                        ComplexNum cnumMult1 = new ComplexNum(realMult1, imagMult1);
+                        ComplexNum cnumMult2 = new ComplexNum(realMult2, imagMult2);
+                        ComplexNum resultMult = clientMult.multCNum(cnumMult1, cnumMult2);
+                        Console.WriteLine(" multCNum(...) = ({0}; {1})", resultMult.real, resultMult.imag);
+                        clientMult.Close();
+                        break;
+                    case 3:
+                        ServiceReference1.CalculatorClient clientDiv = new ServiceReference1.CalculatorClient("WSHttpBinding_ICalculator");
+                        Console.WriteLine("Type first number:\nReal:");
+                        var realDiv1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imagDiv1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Type first number:\nReal:");
+                        var realDiv2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Imaginary:");
+                        var imagDiv2 = float.Parse(Console.ReadLine());
+
+                        ComplexNum cnumDiv1 = new ComplexNum(realDiv1, imagDiv1);
+                        ComplexNum cnumDiv2 = new ComplexNum(realDiv2, imagDiv2);
+                        ComplexNum resultDiv = clientDiv.divCNum(cnumDiv1, cnumDiv2);
+                        Console.WriteLine(" divCNum(...) = ({0}; {1})", resultDiv.real, resultDiv.imag);
+                        clientDiv.Close();
+                        break;
+                    case 4:
+                        ServiceReference3.SuperCalcClient client2 = new ServiceReference3.SuperCalcClient(instanceContext);
+                        Console.WriteLine("Type record number:");
+                        double value1 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("...call of Factorial({0})...", value1);
+                        client2.Factorial(value1);
+                        Console.ReadLine();
+                        client2.Close();
+                        break;
+                    case 5:
+                        ServiceReference3.SuperCalcClient client4 = new ServiceReference3.SuperCalcClient(instanceContext);
+                        Console.WriteLine("Type age:");
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Type name:");
+                        string name = Console.ReadLine();
+                        client4.removeRecord(new PersonalData(name, age));
+
+                        Console.ReadLine();
+                        client4.Close();
+                        break;
+                    case 6:
+                        ServiceReference3.SuperCalcClient clientAdd = new ServiceReference3.SuperCalcClient(instanceContext);
+                        Console.WriteLine("Type age:");
+                        int ageAdd = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Type name:");
+                        string nameAdd = Console.ReadLine();
+                        clientAdd.addRecord(nameAdd, ageAdd);
+                        
+                        Console.ReadLine();
+                        clientAdd.Close();
+                        break;
+                    default:
+                        break;
+                }
+                if (leave)
+                {
+                    break;
+                }
+            }
+
+            
+
+        }
+    }
+}
---
+++ WCFService/4b/WcfServiceHost/Program.cs
@@ -0,0 +1,101 @@
+﻿using CallbackService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService2;
+
+namespace WcfServiceHost
+{
+    class MyData
+    {
+        public static void static_info()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Bartłomiej Złocki 256766");
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.OSVersion.ToString());
+            Console.WriteLine(Environment.Version.ToString());
+            Console.WriteLine(Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString());
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData.static_info();
+            Uri baseAddress = new Uri("http://localhost:8002/wcf");
+            ServiceHost myHost = new ServiceHost(typeof(MyComplexCalc), baseAddress);
+            WSHttpBinding myBinding = new WSHttpBinding();
+            ServiceEndpoint endpoint1 = myHost.AddServiceEndpoint(typeof(ICalculator), myBinding, "endpoint1");
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            myHost.Description.Behaviors.Add(smb);
+
+            Console.WriteLine("\nService endpoint {0}:", endpoint1.Name);
+            Console.WriteLine("Binding: {0}", endpoint1.Binding.ToString());
+            Console.WriteLine("ListenUri: {0}", endpoint1.ListenUri.ToString());
+
+
+            Uri baseAddress2 = new Uri("http://localhost:8002/wcf2");
+            ServiceHost myHost2 = new ServiceHost(typeof(AsyncService), baseAddress2);
+            ServiceEndpoint endpoint2 = myHost2.AddServiceEndpoint(typeof(IAsyncService), myBinding, "endpoint2");
+            myHost2.Description.Behaviors.Add(smb);
+
+            Console.WriteLine("\nService endpoint {0}:", endpoint2.Name);
+            Console.WriteLine("Binding: {0}", endpoint2.Binding.ToString());
+            Console.WriteLine("ListenUri: {0}", endpoint2.ListenUri.ToString());
+
+            Uri baseAddress3 = new Uri("http://localhost:8002/wcf3");
+            ServiceHost myHost3 = new ServiceHost(typeof(MySuperCalc), baseAddress3);
+            WSDualHttpBinding myBinding3 = new WSDualHttpBinding();
+            ServiceEndpoint endpoint3 = myHost3.AddServiceEndpoint(typeof(ISuperCalc), myBinding3, "ThirdService");
+            myHost3.Description.Behaviors.Add(smb);
+
+            Console.WriteLine("\nService endpoint {0}:", endpoint3.Name);
+            Console.WriteLine("Binding: {0}", endpoint3.Binding.ToString());
+            Console.WriteLine("ListenUri: {0}", endpoint3.ListenUri.ToString());
+
+
+            try
+            {
+                myHost.Open();
+                Console.WriteLine("Serwis jest uruchomiony");
+
+                myHost2.Open();
+                Console.WriteLine("--> Async service is running");
+
+                myHost3.Open();
+                Console.WriteLine("--> Callback SuperCalc is running.");
+
+                Console.WriteLine("Naciśnij <Enter> aby zakończyć");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                myHost2.Close();
+                Console.WriteLine("--> Async service finished");
+
+                myHost3.Close();
+                Console.WriteLine("--> Callback SuperCalc finished");
+
+                myHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("Wystąpił wyjątek: {0}", ce.Message);
+                myHost.Abort();
+                myHost2.Abort();
+                myHost3.Abort();
+            }
+
+
+            
+
+        }
+    }
+}
