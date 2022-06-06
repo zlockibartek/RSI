c25b4921be03736d5946b70518660f945f54266b
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2022-06-06 15:15:35 +0200

    remove junk

--- server/src/ResultType.java
+++
@@ -1,8 +0,0 @@
-import java.io.Serializable;
-
-public class ResultType implements Serializable {
-    private static final long serialVersionUID = 102L;
-
-    String result_description;
-    public String result;
-}
